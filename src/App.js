a440fcfb331492bd14aa2ec2626f3b683d541029
Deepak <deepak.malik@borderfree.io>
2022-11-21 19:49:58 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  window.addEventListener("message", (message) => {
+    if ((message.origin = "https://firstdomain.pages.dev/")) {
+      localStorage.setItem("firstDomainId", message.data);
+    }
+  });
   return (
     <>
-      <div>b</div>
+      <div>this page is listening for message</div>
     </>
   );
 }
